99d55083b7018078e4438e0582a1c37ec56c4811
Wereseal <sholto.coulter@proton.me>
2026-09-24 15:29:17 +0100

    added more tests

--- src/towers_of_hanoi.rs
+++ src/towers_of_hanoi.rs
@@ -13,16 +13,25 @@ impl Tower {
     }
 }
 pub struct TowersOfHanoi {
-    count: u32,
+    ring_count: u32,
     towers: [Vec<u32>; 3],
 }
 impl TowersOfHanoi {
     pub fn new(count: u32) -> Self {
         let first: Vec<u32> = (1..=count).rev().collect();
         TowersOfHanoi {
-            count,
+            ring_count: count,
             towers: [ first, Vec::new(), Vec::new() ],
         }
+    }
+    pub fn ring_count(&self) -> u32 {
+        self.ring_count
+    }
+    pub fn towers(&self) -> &[Vec<u32>; 3] {
+        &self.towers
+    }
+    pub fn solved(&self) -> bool {
+        self.towers.iter().map(|tower| if tower.len() == 0 {0} else {1}).sum::<u8>() <= 1
     }
 }
 
@@ -30,6 +39,11 @@ impl TowersOfHanoi {
 mod tests {
     use super::*;
 
+    fn check_ring_count_is_correct(towers: &TowersOfHanoi) -> bool {
+        let real_total: u32 = towers.towers.iter().map(|tower| tower.len() as u32).sum();
+        let reported_total: u32 = towers.ring_count();
+        real_total == reported_total
+    }
     #[test]
     fn tower_get_index_gives_correct_index() {
         let tower_a = Tower::A;
@@ -40,4 +54,11 @@ mod tests {
         assert_eq!(tower_c.get_index(), 2);
     }
     #[test]
+    fn TowersOfHanoi_constructs_correct() {
+        let towers = TowersOfHanoi::new(0);
+        assert!(check_ring_count_is_correct(&towers));
+        assert!(towers.solved());
+    }
+
+
 }
